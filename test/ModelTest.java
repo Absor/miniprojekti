1e976e0c1f99c3ad18c5b42b1ef2fc79097b2f06
Tuomoz <tuomon.email@gmail.com>
2013-04-09 22:06:10 +0300

    Cleaned class + 1 new test

--- test/ModelTest.java
+++ test/ModelTest.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.validator.xml.FieldType;
 import org.junit.*;
 
 import static play.test.Helpers.*;
@@ -7,27 +11,42 @@ import models.*;
 
 public class ModelTest {
 	
+	@Before
+    public void setUp() {
+        start(fakeApplication(inMemoryDatabase()));
+    }
+	
     @Test
     public void findById() {
-        running(fakeApplication(inMemoryDatabase()), new Runnable() {
-           public void run() {
-               Reference refFromDb = Reference.find.byId(1001l);
-               assertThat(refFromDb.author).isEqualTo("testaut1");
-               assertThat(refFromDb.referenceType.id).isEqualTo(1);
-           }
-        });
+       Reference refFromDb = Reference.find.byId(1001l);
+       assertThat(refFromDb.author).isEqualTo("testaut1");
+       assertThat(refFromDb.referenceType.id).isEqualTo(1);
     }
     
     @Test
     public void deleteById() {
-        running(fakeApplication(inMemoryDatabase()), new Runnable() {
-           public void run() {
-               Reference.find.byId(1001l).delete();
-               Reference refFromDb = Reference.find.byId(1l);
-               // can't find anymore
-               assertThat(refFromDb).isNull();
-           }
-        });
+       Reference.find.byId(1001l).delete();
+       Reference refFromDb = Reference.find.byId(1l);
+       // can't find anymore
+       assertThat(refFromDb).isNull();
+    }
+    
+    @Test
+    public void AddingFieldTypesToReferenceType() {
+    	new models.FieldType("Required 1").save();
+    	new models.FieldType("Required 2").save();
+    	new models.FieldType("Optional 1").save();
+    	new models.FieldType("Optional 2").save();
+    	
+    	String[] req = {"Required 1", "Required 2" };
+    	String[] opt = {"Optional 1", "Optional 2"};
+
+    	ReferenceType asd = new ReferenceType(2L, "test", req, opt);
+    	asd.save();
+    	
+        ReferenceType ref = ReferenceType.find.where().eq("id", "2").findUnique();
+        assertThat(ref.requiredFields.get(1).fieldName).isEqualTo("Required 2");
+        assertThat(ref.optionalFields.get(0).fieldName).isEqualTo("Optional 1");
     }
     
 }
