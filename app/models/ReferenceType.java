2f6f9d64e81844c5c90d1573bfd29b8571c6a554
Tuomoz <tuomon.email@gmail.com>
2013-04-09 21:59:16 +0300

    Updated reference types to use field types

--- app/models/ReferenceType.java
+++ app/models/ReferenceType.java
@@ -1,20 +1,52 @@
 package models;
 
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 
+import play.db.ebean.Model;
 import play.db.ebean.Model.Finder;
 
 @Entity
-public class ReferenceType {
+public class ReferenceType extends Model{
 
 	@Id
 	public Long id;
 	
 	public String name;
+	
+	@ManyToMany(cascade = CascadeType.REMOVE)
+	@JoinTable(name = "RequiredFields")
+	public List<FieldType> requiredFields = new ArrayList<FieldType>();
+	
+	@ManyToMany(cascade = CascadeType.REMOVE)
+	@JoinTable(name = "OptionalFields")
+	public List<FieldType> optionalFields = new ArrayList<FieldType>();
+	
+	public ReferenceType(Long id, String name, String[] required, String[] optional) {
+		this.id = id;
+		this.name = name;
+		
+		for (String field : required) {
+			requiredFields.add(FieldType.find.ref(field));
+		}
+		for (String field : optional) {
+			optionalFields.add(FieldType.find.ref(field));
+		}
+	}
+	
+	public void save() {
+		super.save();
+		this.saveManyToManyAssociations("requiredFields");
+		this.saveManyToManyAssociations("optionalFields");
+	}
 	
 	public static Finder<Long, ReferenceType> find = new Finder<Long, ReferenceType>(
 			Long.class, ReferenceType.class);
@@ -29,4 +61,5 @@ public class ReferenceType {
         }
         return options;
     }
+	
 }
