e6771c1a2b603dc86f5f1313d82654778e0b434f
Tuomoz <tuomon.email@gmail.com>
2013-04-10 13:03:35 +0300

    Updated tests to use test-data.yml

--- test/FunctionalTest.java
+++ test/FunctionalTest.java
@@ -1,44 +1,51 @@
 import org.junit.*;
 
+import com.avaje.ebean.Ebean;
+
 import java.util.*;
 
 import play.mvc.*;
 import play.test.*;
+import play.libs.Yaml;
 import play.libs.F.*;
 
 import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.*;
 
 public class FunctionalTest {
-
+	
+	@Before
+    public void setUp() {
+        start(fakeApplication(inMemoryDatabase()));
+        
+        // Loading test references
+        Map<String,List<Object>> all = (Map<String,List<Object>>)Yaml.load("test-data.yml");
+        Ebean.save(all.get("references"));
+    }
+	
     @Test
     public void redirectsToReferences() {
-        running(fakeApplication(inMemoryDatabase()), new Runnable() {
-           public void run() {
+
                Result result = callAction(controllers.routes.ref.Application.index());
 
                assertThat(status(result)).isEqualTo(SEE_OTHER);
                assertThat(redirectLocation(result)).isEqualTo("/references");
-           }
-        });
+
     }
     
     @Test
     public void listsReferencesOnFrontPage() {
-        running(fakeApplication(inMemoryDatabase()), new Runnable() {
-           public void run() {
+
                Result result = callAction(controllers.routes.ref.Application.list());
 
                assertThat(status(result)).isEqualTo(OK);
                assertThat(contentAsString(result)).contains("References list");
-           }
-        });
+
     }
     
     @Test
     public void createANewReference() {
-        running(fakeApplication(inMemoryDatabase()), new Runnable() {
-            public void run() {
+
                 Result result = callAction(controllers.routes.ref.Application.save());
 
                 assertThat(status(result)).isEqualTo(BAD_REQUEST);
@@ -76,20 +83,17 @@ public class FunctionalTest {
                 assertThat(status(result)).isEqualTo(OK);
                 assertThat(contentAsString(result)).contains("badbadbad");
                 
-            }
-        });
+
     }
     
     @Test
     public void downloadBibtex() {
-    	running(fakeApplication(inMemoryDatabase()), new Runnable() {
-            public void run() {
+
                 Result result = callAction(controllers.routes.ref.Application.generateBib());
 
                 assertThat(status(result)).isEqualTo(OK);
                 assertThat(contentAsString(result)).contains("author = {\\aa\\\"{o}\\\"{a}\\{\\\"\\$},");
-            }
-         });
+
     }
     
 }
