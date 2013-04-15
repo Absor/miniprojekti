ed46d1006e30fd74b9dafe19563b82d81bf77119
Heikki Haapala <heikki.haapala@helsinki.fi>
2013-04-15 13:28:52 +0300

    code coverage can be generated with play jacoco:cover now

--- project/Build.scala
+++ project/Build.scala
@@ -2,18 +2,25 @@ import sbt._
 import Keys._
 import PlayProject._
 
+import de.johoop.jacoco4sbt.JacocoPlugin._
+
 object ApplicationBuild extends Build {
 
     val appName         = "Reference database"
     val appVersion      = "1.0"
+      
+      lazy val s = Defaults.defaultSettings ++ Seq(jacoco.settings:_*)
 
     val appDependencies = Seq(
       // Add your project dependencies here,
       "postgresql" % "postgresql" % "9.1-901-1.jdbc4"
     )
 
-    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
-      // Add your own project settings here      
+    val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA, settings = s).settings(
+    		// Add your own project settings here      
+    		parallelExecution in jacoco.Config := false // ,
+    		// jacoco.includes in jacoco.Config := Seq("*"),
+    		// jacoco.excludes in jacoco.Config := Seq("*")
     )
 
 }
